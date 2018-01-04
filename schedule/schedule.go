f294bebbc09761e6d42f8f57a6f6625dcae25fbe
Niten <niten2@yandex.ru>
2018-01-04 13:40:58 +0700

    edit schedule

--- schedule/schedule.go
+++ schedule/schedule.go
@@ -10,10 +10,10 @@ import (
 
 func Run (bot *tgbotapi.BotAPI) {
 	// gocron.Every(2).Seconds().Do(bot_api.SendMessage, bot)
-	gocron.Every(1).Day().At("10:30").Do(bot_api.SendMessage, bot)
+	gocron.Every(1).Day().At("10:00").Do(bot_api.SendMessage, bot)
 
   gocron.Start()
 
-  log.Printf("start schedule every one seconds")
+  log.Printf(`start schedule every day at 10:00`)
 }
 
