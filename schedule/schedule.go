6d45d71d1c2a8ab791796a1d37fed6dc48fe557a
Niten <niten2@yandex.ru>
2018-01-04 13:37:33 +0700

    add gitignore, fix schedule

--- schedule/schedule.go
+++ schedule/schedule.go
@@ -10,7 +10,7 @@ import (
 
 func Run (bot *tgbotapi.BotAPI) {
 	// gocron.Every(2).Seconds().Do(bot_api.SendMessage, bot)
-	gocron.Every(1).Day().At("10:30").Do(task)
+	gocron.Every(1).Day().At("10:30").Do(bot_api.SendMessage, bot)
 
   gocron.Start()
 
